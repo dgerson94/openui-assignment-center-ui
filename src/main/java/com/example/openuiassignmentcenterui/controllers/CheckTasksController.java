aef7dcaf31317880e689f0855b07e0a39de6f022
dov_gerson <dov.gerson@anyword.com>
2023-03-21 08:19:56 +0200

    forward flow works till switching controllers
    Signed-off-by: dov_gerson <dov.gerson@anyword.com>

--- src/main/java/com/example/openuiassignmentcenterui/controllers/CheckTasksController.java
+++ src/main/java/com/example/openuiassignmentcenterui/controllers/CheckTasksController.java
@@ -3,12 +3,10 @@ package com.example.openuiassignmentcenterui.controllers;
 import com.example.openuiassignmentcenterui.helpers.Controller;
 import com.example.openuiassignmentcenterui.helpers.Error;
 import com.example.openuiassignmentcenterui.helpers.Https;
-import com.example.openuiassignmentcenterui.models.Course;
-import com.example.openuiassignmentcenterui.models.Professor;
-import com.example.openuiassignmentcenterui.models.Student;
-import com.example.openuiassignmentcenterui.models.Task;
+import com.example.openuiassignmentcenterui.models.*;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -29,9 +27,18 @@ public class CheckTasksController {
     private ListView<String> courseListView;
     private ArrayList<Course> professorCourses;
 
+    private ArrayList<Task> tasks;
+
+    private ArrayList<Student> students;
+
     private String pickedCourse;
     private String pickedTask;
     private String pickedStudent;
+
+    private String courseId;
+
+    private String taskId;
+
     @FXML
     private Label studentLabel;
 
@@ -45,53 +52,73 @@ public class CheckTasksController {
         TODO: Cam go back with a back button. Need to add back button and disable lists after selection.
          */
     void continueButtonPressed(ActionEvent event) throws IOException {
-        String pickedCourse = courseListView.getSelectionModel().getSelectedItem();
-        if (pickedCourse == null){
-            Error e = new Error("No course selected", "You did not select a course, please select a course.");
-            e.raiseError();
+        // In case that studentListView is enabled we have all the information, go to checkTask screen
+        if (!studentListView.isDisabled()) {
+            pickedStudent = studentListView.getSelectionModel().getSelectedItem();
+            if (pickedStudent == null){
+                Error e = new Error("No students in this course.", "Call the rector and complain that no one signed up for your course.");
+                e.raiseError();
+            } else {
+                //implement opening check task controller with correct info.
+            }
         } else {
-            String courseId = Controller.getCourseId(pickedCourse,professorCourses);
-            StringBuffer responseTasks = Https.httpGet(user.getId(), user.getPassword(), Https.PROFESSOR, URL_COURSES + "/" + courseId + "/tasks");
-            if (!responseTasks.toString().equals("[]")) {
-                TypeToken<ArrayList<Task>> courseType = new TypeToken<>() {
-                };
-                ArrayList<Task> tasks = new Gson().fromJson(String.valueOf(responseTasks), courseType);
-                taskListView = new ListView<>(Controller.createObservableList(tasks.size()));
-                String pickedTask = taskListView.getSelectionModel().getSelectedItem();
-                if (pickedTask == null){
+            //In Case that taskListView is enabled we want to take the selected task and get relevant students.
+            if (!taskListView.isDisabled()) {
+                pickedTask = taskListView.getSelectionModel().getSelectedItem();
+                if (pickedTask == null) {
                     Error e = new Error("No task selected", "You did not select a task, please select a course.");
                     e.raiseError();
-                } else{
-                    String taskId = Controller.getTaskId(pickedTask,tasks);
-                    StringBuffer responseStudents = Https.httpGet(user.getId(),user.getPassword(),Https.PROFESSOR,URL_COURSES + "/" + courseId + "/tasks" + taskId);
-                    if (!responseStudents.toString().equals("[]")){
-                        TypeToken<ArrayList<Student>> studentType = new TypeToken<>() {
+                } else {
+                    taskId = Controller.getTaskId(pickedTask, tasks);
+                    StringBuffer responseStudents = Https.httpGet(user.getId(), user.getPassword(), Https.PROFESSOR, URL_COURSES + "/" + courseId + "/tasks/" + taskId + "/submissions");
+                    if (!responseStudents.toString().equals("[]")) {
+                        TypeToken<ArrayList<Submission>> submissionType = new TypeToken<>() {
                         };
-                        ArrayList<Student> students = new Gson().fromJson(String.valueOf(responseStudents), studentType);
-                        studentListView = new ListView<>(Controller.createObservableStudentList(students));
-                        String pickedStudent = studentListView.getSelectionModel().getSelectedItem();
-                        //flow here not finished, stopped to change flow logic need to add call to get students info and switch controllers.
-                    } else {
-                        Error e = new Error("No students in this course.", "Call the rector and complain that no one signed up for your course.");
-                        e.raiseError();
+                        ArrayList<Submission> submissions = new Gson().fromJson(String.valueOf(responseStudents), submissionType);
+                        update_lists(studentListView,taskListView, Controller.createObservableList(students.size()));
                     }
                 }
-
-
             } else {
-                //if there is an empty string - error, can't check before setting tasks
-                Error e = new Error("No tasks.", "You did not set any requirements for this course. Please first set requirements.");
-                e.raiseError();
+                //If we have reached here than only the courseListView is enabled, we will get the tasks.
+                pickedCourse = courseListView.getSelectionModel().getSelectedItem();
+                if (pickedCourse == null) {
+                    Error e = new Error("No course selected", "You did not select a course, please select a course.");
+                    e.raiseError();
+                } else {
+                    courseId = Controller.getCourseId(pickedCourse, professorCourses);
+                    StringBuffer responseTasks = Https.httpGet(user.getId(), user.getPassword(), Https.PROFESSOR, URL_COURSES + "/" + courseId + "/tasks");
+                    if (!responseTasks.toString().equals("[]")) {
+                        TypeToken<ArrayList<Task>> courseType = new TypeToken<>() {
+                        };
+                        tasks = new Gson().fromJson(String.valueOf(responseTasks), courseType);
+                        update_lists(taskListView, courseListView, Controller.createObservableList(tasks.size()));
+                    }
+                }
             }
         }
+    }
 
+    private void update_lists(ListView<String> newList, ListView<String> oldList, ObservableList<String> newOList) {
+        newList.setItems(newOList);
+        newList.refresh();
+        newList.setDisable(false);
+        oldList.setDisable(true);
     }
 
 
     @FXML
     void backButtonPressed(ActionEvent event) throws IOException {
-        //TODO: Needs to be updated for flow with disabled lists.
-        SceneController.switchToScene(event, "professor_dashboard.fxml");
+        if (!studentListView.isDisabled()){
+            students = null;
+            studentListView.setDisable(true);
+            taskListView.setDisable(false);
+        } else if (!taskListView.isDisabled()) {
+            tasks = null;
+            taskListView.setDisable(true);
+            courseListView.setDisable(false);
+        } else  {
+            SceneController.switchToScene(event, "professor_dashboard.fxml");
+        }
     }
 
     public void setProfessor(Professor professor) throws IOException {
