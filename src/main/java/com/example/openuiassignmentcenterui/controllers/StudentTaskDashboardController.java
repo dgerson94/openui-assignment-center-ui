1a3a20592b5423c0c1492a27957814b3287d6ae7
dov_gerson <dov.gerson@anyword.com>
2023-04-16 22:29:11 +0300

    remove redundant import
    Signed-off-by: dov_gerson <dov.gerson@anyword.com>

--- src/main/java/com/example/openuiassignmentcenterui/controllers/StudentTaskDashboardController.java
+++ src/main/java/com/example/openuiassignmentcenterui/controllers/StudentTaskDashboardController.java
@@ -12,7 +12,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
-import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
